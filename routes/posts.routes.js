78d7342d9b95c2c3939a5d8ef9f7c4ff09e1c0d7
LeoD3v <leoargusa1991@gmail.com>
2023-12-05 22:25:47 +0100

    perfildeusuaro-server profile page half done\

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { isAuthenticated } = require('../middleware/jwt.middleware')
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 const Post = require("../models/Post.model");
 const City = require("../models/City.model");
-const fileUploader = require('../config/cloudinary.config');
+const fileUploader = require("../config/cloudinary.config");
 
 router.use(isAuthenticated);
 
 router.get("/:cityId", async (req, res) => {
   const cityId = req.params.cityId;
-  console.log('Received CityId:', cityId); 
+  console.log("Received CityId:", cityId);
   try {
     const city = await City.findById(cityId);
 
     if (!city) {
-      return res.status(404).json({ error: 'City not found' });
+      return res.status(404).json({ error: "City not found" });
     }
-    const posts = await Post.find({ _id: { $in: city.posts } }).populate('likes').populate('user');
+    const posts = await Post.find({ _id: { $in: city.posts } })
+      .populate("likes")
+      .populate("user");
 
     res.json({ city, posts });
   } catch (error) {
@@ -25,16 +27,16 @@ router.get("/:cityId", async (req, res) => {
   }
 });
 
-router.post("/:cityId", fileUploader.single('photo'),  async (req, res) => {
+router.post("/:cityId", fileUploader.single("photo"), async (req, res) => {
   const { cityId } = req.params;
   const { caption, user } = req.body;
-  console.log('Received CityId:', cityId); 
+  console.log("Received CityId:", cityId);
 
   try {
     const city = await City.findById(cityId);
-    console.log('City:', city);
+    console.log("City:", city);
     if (!city) {
-      return res.status(404).json({ error: 'City not found' });
+      return res.status(404).json({ error: "City not found" });
     }
 
     const newPost = new Post({
@@ -43,17 +45,35 @@ router.post("/:cityId", fileUploader.single('photo'),  async (req, res) => {
       user,
       city: cityId,
     });
-   await newPost.save();
+    await newPost.save();
 
     city.posts.push(newPost._id);
     await city.save();
- 
+
     res.status(201).json(newPost);
   } catch (error) {
-    
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+router.get("/cities/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  console.log("Received userId:", userId);
+  try {
+    const post = await Post.find({ user: userId })
+      .populate("city")
+      .populate("comments")
+      .populate("user");
 
+    if (!post) {
+      return res.status(404).json({ error: "post not found" });
+    }
+    // const posts = await Post.find({ _id: { $in: city.posts } }).populate('likes').populate('user');
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 module.exports = router;
